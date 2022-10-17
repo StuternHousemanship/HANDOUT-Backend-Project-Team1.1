92a39742bda486bbd88efdff981e415b231a2a93
saslamp <sadiqasg@gmail.com>
2022-10-17 12:56:00 +0100

    bug fixes

--- src/config/nodemailerConfig.ts
+++ src/config/nodemailerConfig.ts
@@ -25,10 +25,12 @@ export const sendConfirmEMail = async (
         subject: "Handout Confirmation Email",
         html: `
           <h2>Hello ${name}!</h2>
-          <p>Thank you for signing up to Handout. Please confirm your email by clicking on the following link</p>
-          <a target="_blank" href="http://localhost:3000/auth/confirm/${verificationCode}"> Click here</a>
+            <p>Thank you for signing up to Handout.</p>
+            <p style="text-align:center;margin-top:10px">Please confirm your email with this link: <a target="_blank" href="http://localhost:3000/auth/confirm/${verificationCode}"> Click here</a></p>
           </div>`,
     });
-
     console.log("Message sent: %s", info.messageId);
 };
+
+
+// <a target="_blank" href="http://localhost:3000/auth/confirm/${verificationCode}"> Click here</a>
