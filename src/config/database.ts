c78bd17d50a392711656f339445dc5ad32265aa7
saslamp <sadiqasg@gmail.com>
2022-10-18 09:43:33 +0100

    project setup

---
+++ src/config/database.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { ConnectionOptions } from "tls";
+
+mongoose.set("debug", true); // this logs mongo query to terminal
+
+const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+
+const database = async () => {
+    await mongoose
+        .connect(
+            "mongodb://localhost:27017/handout",
+            connectionParams as ConnectionOptions
+        )
+        .then(() => {
+            console.log("Connected to Handout DB on localhost!");
+        })
+        .catch((err) => {
+            console.error(`Error connecting to the database. n${err}`);
+        });
+};
+
+export default database;
