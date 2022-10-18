127039cf6d09ef06e3403dd68968239f6531ba68
saslamp <sadiqasg@gmail.com>
2022-10-18 09:58:09 +0100

    ft(create-user): user can create account user receives account verification email user can verify account via email

---
+++ src/interfaces/userType.ts
@@ -0,0 +1,11 @@
+type UserType = {
+    firstName: string;
+    lastName: string;
+    email: string;
+    mobile: number;
+    password: string;
+    active: boolean;
+    verificationCode: string;
+};
+
+export default UserType
