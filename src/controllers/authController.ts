51fac540518c5938659e8e5c8265fc13bbfd9f09
saslamp <sadiqasg@gmail.com>
2022-10-18 15:42:35 +0100

    implemented feedback on create user

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -4,7 +4,8 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { sendConfirmEMail } from "../config/nodemailerConfig";
 import { generateCode } from "../config/generateCode";
-import * as dotenv from 'dotenv'
+import * as dotenv from "dotenv";
+import { findUser } from "../services/userExists";
 
 dotenv.config();
 
@@ -21,55 +22,26 @@ export const createUser = async (req: Request, res: Response) => {
         verificationCode: code,
     });
 
-    try {
-        const emailCheck = await User.findOne({
-            $or: [{ email: newUser.email }],
-        });
-        if (emailCheck) {
-            return res.status(400).send({
-                message:
-                    "This Email is already in use, please login to your account",
-            });
-        }
+    const check = await findUser("email", newUser.email);
+    if (check) return res.status(400).json("User already exists, please login");
 
-        newUser
-            .save()
-            .then((user) => {
-                sendConfirmEMail(
-                    user.firstName,
-                    user.email,
-                    user.verificationCode
-                );
-                const message = "Check your email for confirmation!";
-                res.status(201).json({ message, user });
-            })
-            .catch((error) => {
-                res.status(404).json(error);
-            });
-    } catch (error) {
-        res.status(400).send(error);
-    }
+    await newUser.save();
+    sendConfirmEMail(
+        newUser.firstName,
+        newUser.email,
+        newUser.verificationCode
+    );
+    return res.status(201).json(newUser);
 };
 
-export const verifyUserEmail = (req: Request, res: Response) => {
-    User.findOne({
-        verificationCode: req.params.verificationCode,
-    })
-        .then((user) => {
-            if (!user) {
-                return res.status(404).send({ message: "User Not found." });
-            }
+export const verifyUserEmail = async (req: Request, res: Response) => {
+    const user = await findUser(
+        "verificationCode",
+        req.params.verificationCode
+    );
+    if (!user) return res.status(404).send({ message: "User Not found." });
 
-            user.active = true;
-            user.save((err) => {
-                if (err) {
-                    res.status(500).send({ message: err });
-                    return;
-                }
-                return res
-                    .status(200)
-                    .send("Email verified! Close this tab and login");
-            });
-        })
-        .catch((e) => console.log("error", e));
+    user.active = true;
+    await user.save();
+    res.status(200).send("Email verified! Close this tab and login");
 };
