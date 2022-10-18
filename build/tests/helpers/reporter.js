874f29320da1aa663504ad0f321f2da47751f449
Azutech <azunna.onugha@gmail.com>
2022-10-18 17:07:08 +0100

    ft-testDev-branch unit test was implemented centarl error handler was added [DELIVER #TEST BRANCH]

--- build/tests/helpers/reporter.js
+++ build/tests/helpers/reporter.js
@@ -1,4 +1,4 @@
-"use strict";
+
 Object.defineProperty(exports, "__esModule", { value: true });
 const jasmine_spec_reporter_1 = require("jasmine-spec-reporter");
 class CustomProcessor extends jasmine_spec_reporter_1.DisplayProcessor {
