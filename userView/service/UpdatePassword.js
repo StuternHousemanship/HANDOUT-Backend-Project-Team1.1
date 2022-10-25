65c69468091b004ac3d081a28d5e490d9e4be78e
Toluwalase Abigail <toluabby12@gmail.com>
2022-10-25 22:34:21 +0100

    added user view

---
+++ userView/service/UpdatePassword.js
@@ -0,0 +1,22 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, NotFoundError, UnAuthenticatedError } = require('../errors/index');
+
+const updateUserPassword = async (req, res) => {
+    const { oldPassword, newPassword } = req.body;
+    if (!oldPassword || !newPassword) {
+      throw new BadRequestError('Please provide both values');
+    }
+    const user = await User.findOne({ _id: req.user.userId });
+  
+    const isPasswordCorrect = await user.comparePassword(oldPassword);
+    if (!isPasswordCorrect) {
+      throw new UnAuthenticatedError ('Invalid Credentials');
+    }
+    user.password = newPassword;
+  
+    await user.save();
+    res.status(StatusCodes.OK).json({ msg: 'Success! Password Updated.' });
+  };
+
+  module.exports= updateUserPassword;
