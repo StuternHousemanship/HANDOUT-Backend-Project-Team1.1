777114d9236b00ef9dea981efda7ed95700270b3
Azutech <azunna.onugha@gmail.com>
2022-11-01 14:01:52 +0100

    bg-reset-password

--- src/services/Auth/index.ts
+++ src/services/Auth/index.ts
@@ -4,10 +4,14 @@ import { generateCode } from "../../services/generateCode";
 import bcrypt from "bcrypt";
 import { AuthRepository } from "../../Repository/Auth";
 import User from "../../models/userModel";
-import { findUser } from "../../services/userExists";
-import { sendConfirmEMail } from "../../services/nodemailerEmail";
-import { verifyPassword } from "../../services/verifyPassword";
+import { findUser } from "../userExists";
+import { sendConfirmEMail } from "../Email";
+import { verifyPassword } from "../verifyPassword";
 import jwt from "jsonwebtoken";
+import joi from 'joi'
+import {tokens} from '../../models/tokenModel'
+import {digitalCode} from '../../services/digitalCode'
+import {sendForgotpassword} from '../Email'
 
 dotenv.config();
 
@@ -78,3 +82,88 @@ export const loginService = async (req: Request, res: Response) => {
     res.cookie("handout_token", token);
     res.status(200).json({ message: "Login Succesful", token });
 };
+
+export const forgotPasswordService = async (req: Request, res: Response) => {
+
+  const code = digitalCode()
+  console.log(code)
+    try {
+      const result = joi.object({email: joi.string().email().required()})
+      const { error } = result.validate(req.body)
+      if (error)
+      return res.json({
+        status: 'error',
+        error: 'Enter a valid email'
+      })
+
+      const user = await User.findOne({email: req.body.email})
+      if(!user)
+      return res.json({
+        status: "error",
+        error: "This email does not exist"
+      })
+  
+      let token = await tokens.findOne({userId: user.id});
+      if (!token) {
+        token = await new tokens({
+          userId: user.id,
+          token: code
+        }).save()
+      }
+      
+      await sendForgotpassword("User", user.email, code)
+
+      res.json({
+        status: 200,
+        success: "password reset link sent to your email account"
+      })
+      
+
+    } catch (err) {
+      console.log(err)
+    }
+}
+
+export const resetPass = async (req: Request, res: Response) => {
+ 
+    try {
+      const result = joi.object({ password: joi.string().required() });
+      const { error } = result.validate(req.body);
+      if (error)
+        return res.json({
+          status: 400,
+          error: "invalid password format",
+        });
+  
+      const user = await User.findById(req.params.userId);
+      if (!user)
+        return res.json({
+          status: 400,
+          error: "invalid link or expired",
+        });
+  
+      const token = await tokens.findOne({
+        userId: user.id,
+        toking: req.params.token,
+      });
+      if (!token)
+        return res.json({
+          status: 400,
+          error: "invalid link or expired",
+        });
+  
+      user.password = req.body.password;
+      await user.save();
+      await token.delete();
+  
+      return res.json({
+        status: 200,
+        error: "password reset successfully",
+      });
+    } catch (error) {
+      res.json({
+        status: 400,
+        error: "something went wrong",
+      });
+    }
+  };
